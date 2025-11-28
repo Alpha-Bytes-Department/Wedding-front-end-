6646ba92d6b38ab8d43983d62715f9dfdeab4208
Joy Sutradhar <joysd.1005@fec.edu.bd>
2025-11-29 00:42:31 +0600

    polishing the site

--- src/Pages/DashBoard/Bookings/Bookings.tsx
+++ src/Pages/DashBoard/Bookings/Bookings.tsx
@@ -15,10 +15,12 @@ interface Booking {
   scheduleDate: string;
   officiantName: string;
   eventId?: string;
+  price?: number;
   message?: string;
 }
 
 interface Ceremony {
+  price: number;
   _id: string;
   title: string;
   eventDate: string;
@@ -331,12 +333,6 @@ const Bookings = () => {
                         >
                           Manage Agreement
                         </button>
-                        {/* <button
-                          onClick={() => navigate("/dashboard/discussions")}
-                          className="bg-[#D4AF371A] text-[#91c21f] border-1 border-primary text-sm px-5 py-2 rounded-lg font-medium hover:bg-primary/90 hover:text-white transition-colors"
-                        >
-                          Go Chat
-                        </button> */}
                       </div>
                     </div>
                   ))
@@ -371,12 +367,6 @@ const Bookings = () => {
                         >
                           Manage Agreement
                         </button>
-                        {/* <button
-                          onClick(() => navigate("/dashboard/discussions")}
-                          className="bg-[#D4AF371A] text-[#91c21f] border-1 border-primary text-sm px-5 py-2 rounded-lg font-medium hover:bg-primary/90 hover:text-white transition-colors"
-                        >
-                          Go Chat
-                        </button> */}
                       </div>
                     </div>
                   ))
@@ -408,26 +398,27 @@ const Bookings = () => {
                   completedCeremonies.map((m) => (
                     <div
                       key={m._id}
-                      className="flex flex-col md:flex-row gap-5 items-center justify-between border border-primary rounded-lg px-4 py-3"
+                      className="flex  flex-col md:flex-row gap-5 items-center justify-between border border-primary rounded-lg px-4 py-3"
                     >
-                      <div>
-                        <div className="font-medium flex gap-2 items-center text-gray-900">
+                      <div className="font-medium flex gap-2 items-center text-gray-900">
+                        {getImage(m.userId) ? (
                           <img
                             src={getImage(m.userId)}
                             className="size-12 rounded-full"
                             alt=""
                           />
-                          {m.title}
-                        </div>
-                        <div className="text-sm text-gray-500">
-                          {new Date(m.eventDate).toLocaleDateString("en-GB")} ·
-                          Officiant: {m.officiantName}
-                        </div>
+                        ) : (
+                          <div className="size-12 rounded-full bg-amber-700 flex items-center justify-center text-white font-bold">
+                            {m.title.charAt(0).toUpperCase()}
+                          </div>
+                        )}
+
+                        {m.title}
                       </div>
-                      <div className="flex items-center gap-2">
-                        <PdfMaker eventId={m._id} />
-                        {/* <button className="bg-[#D4AF371A] border-1 border-primary text-primary text-sm px-5 py-2 rounded-lg font-medium hover:bg-primary/90 transition-colors">
-                        </button> */}
+                      <p className="font-bold py-1 px-3 rounded-full border-amber-500 border-2">value : ${m.price}</p>
+                      <div className="text-sm text-gray-500">
+                        {new Date(m.eventDate).toLocaleDateString("en-GB")} ·
+                        Officiant: {m.officiantName}
                       </div>
                     </div>
                   ))
@@ -437,22 +428,24 @@ const Bookings = () => {
                       key={m._id}
                       className="flex  flex-col md:flex-row gap-5 items-center justify-between border border-primary rounded-lg px-4 py-3"
                     >
-                      <div>
-                        <div className="font-medium flex gap-2 items-center text-gray-900">
+                      <div className="font-medium flex gap-2 items-center text-gray-900">
+                        {getImage(m.userId) ? (
                           <img
                             src={getImage(m.userId)}
                             className="size-12 rounded-full"
                             alt=""
                           />
-                          {m.title}
-                        </div>
-                        <div className="text-sm text-gray-500">
-                          {new Date(m.eventDate).toLocaleDateString("en-GB")} ·
-                          Officiant: {m.officiantName}
-                        </div>
+                        ) : (
+                          <div className="size-12 rounded-full bg-amber-700 flex items-center justify-center text-white font-bold">
+                            {m.title.charAt(0).toUpperCase()}
+                          </div>
+                        )}
+
+                        {m.title}
                       </div>
-                      <div className="flex items-center gap-2">
-                        <PdfMaker eventId={m._id} />
+                      <div className="text-sm text-gray-500">
+                        {new Date(m.eventDate).toLocaleDateString("en-GB")} ·
+                        Officiant: {m.officiantName}
                       </div>
                     </div>
                   ))
